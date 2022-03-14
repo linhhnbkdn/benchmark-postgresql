a3777c86649fd42b743539c803086c4610898567
Linh Hoang <linh.hoang@parcelperform.com>
2022-03-14 14:40:37 +0700

    Update config celery

--- src/benchmark_pg/celery_conf/config.py
+++ src/benchmark_pg/celery_conf/config.py
@@ -1,8 +1,8 @@
 import os
 # File config for celery
 
-broker_url = 'redis://redis:6379/0'
-result_backend = 'redis://redis:6379/1'
+broker_url = 'redis://127.0.0.1:6379/0'
+result_backend = 'redis://127.0.0.1:6379/1'
 
 
 CELERY_IMPORTS=("api.views",)
